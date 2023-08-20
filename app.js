9057ce498a96609977381f3251478b043c9effb0
MohammadReza <mohammadreza.jafari.hp@gmail.com>
2023-08-20 22:19:32 +0330

    add script to html and run game with alert!

---
+++ app.js
@@ -0,0 +1,82 @@
+let userScore = 0;
+let cpuScore = 0;
+
+const userScore_span = document.getElementById("user-score");
+const cpuScore_span = document.getElementById("cpu-score");
+
+const restart = document.getElementById("restart");
+
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+function getCpuChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function win(userChoice, cpuChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  alert("You win!");
+}
+
+function lose(userChoice, cpuChoice) {
+  cpuScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  alert("You Lose!");
+}
+
+function draw(userChoice, cpuChoice) {
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  alert("Draw!!!");
+}
+
+function play(userChoice) {
+  const cpuChoice = getCpuChoice();
+  switch (userChoice + cpuChoice) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      win(userChoice, cpuChoice);
+      break;
+    case "rockpaper":
+    case "paperscissors":
+    case "scissorsrock":
+      lose(userChoice, cpuChoice);
+      break;
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      draw(userChoice, cpuChoice);
+      break;
+  }
+}
+
+function app() {
+  rock_div.addEventListener("click", function () {
+    play("rock");
+  });
+
+  paper_div.addEventListener("click", function () {
+    play("paper");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    play("scissors");
+  });
+}
+
+function restartGame() {
+  userScore = 0;
+  cpuScore = 0;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+}
+restart.addEventListener("click", restartGame);
+
+app();
